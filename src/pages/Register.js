4ef41a109c4041e0effea4f01c4c5b9452c84492
aliakbar beigi <o.beygi@gmail.com>
2022-08-29 10:16:43 +0430

    register and form row

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { FormRow, Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [values, setValues] = useState(initialState);
 
-export default Register
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/*name field*/}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/*email field*/}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/*password field*/}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button className="btn btn-block">submit</button>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Register;
